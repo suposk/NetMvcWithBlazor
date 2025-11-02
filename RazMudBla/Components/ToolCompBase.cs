4278dd6c18d5840280f6efeeb28f0f4724af3e52
Jan Supolik <jan.supolik@hotmail.com>
2025-11-02 16:09:12 +0100

    Refactor ToolComp and add ToolCompBase class
    Refactored ToolComp.razor to inherit from the new ToolCompBase class, which now handles button click logic and loading state. Updated loading message display to be conditional. Added a new folder `Components\Features\` to the project structure in RazMudBla.csproj.

---
+++ RazMudBla/Components/ToolCompBase.cs
@@ -0,0 +1,18 @@
+﻿namespace RazMudBla.Components;
+
+public class ToolCompBase : CompBase
+{
+    public string ButtonText { get; set; } = "Click Me";
+    public int ClickCount { get; set; }
+
+    public async Task OnButtonClicked()
+    {
+        // Tool method logic here
+        ClickCount += 1;
+        ShowLoading($"Clicked {ClickCount}.");
+        ButtonText = $"{ClickCount}. Click Me Again";
+        await Task.Delay(TimeSpan.FromSeconds(1));
+
+        HideLoading();
+    }
+}
